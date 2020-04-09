c7294dfa6310b4fc65efa5ab62a8c3b93063d34a
shinsung <tlstjd436@naver.com>
2020-04-10 01:04:29 +0900

    chromedriver 설치 및 연동

--- SWExpertCrawling.py
+++ SWExpertCrawling.py
@@ -1,2 +1,7 @@
-a =10
-print(a)
+from selenium import webdriver
+
+#webdriver가 있어야 막히지 않음.
+driver = webdriver.Chrome("./chromedriver.exe")
+
+#web crwaling 주소 삽입
+driver.get("https://swexpertacademy.com/main/main.do")
